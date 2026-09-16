3cbfd0448cfca5cd9b526f3ef1529d45c0b34540
Amanda Felix <amanda.felix@catskillet.com>
2026-09-16 15:41:45 -0300

    :milky_way: feat: Decks das cartas do jogo

---
+++ src/data/aroundWorld.ts
@@ -0,0 +1,11 @@
+import image1 from '../assets/images/themes/Around the world/1.svg'
+import image2 from '../assets/images/themes/Around the world/2.svg'
+import image3 from '../assets/images/themes/Around the world/3.svg'
+import image4 from '../assets/images/themes/Around the world/4.svg'
+
+export const aroundWorldImages = [
+  { id: 'world-1', image: image1 },
+  { id: 'world-2', image: image2 },
+  { id: 'world-3', image: image3 },
+  { id: 'world-4', image: image4 },
+]
